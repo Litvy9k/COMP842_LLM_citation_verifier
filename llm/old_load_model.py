1a495858456a20a6d9cccacd4c8c2ff12adac215
Litvy9k <hyliu.amoiensis@gmail.com>
2025-10-13 15:52:47 +1300

    final

--- llm/old_load_model.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_id = "mistralai/Mistral-7B-Instruct-v0.3"
-tokenizer = AutoTokenizer.from_pretrained(model_id, token="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")
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype="auto", token="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")
